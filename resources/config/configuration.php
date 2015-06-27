ab35b544e3ffabc5ad5b6125780fdebab9996caa
Ryan Thompson <service@aiwebsystems.com>
2015-06-27 06:18:21 -0500

    Disabled when editing

--- resources/config/configuration.php
+++ resources/config/configuration.php
@@ -2,8 +2,9 @@
 
 return [
     'privacy' => [
-        'type'   => 'anomaly.field_type.select',
-        'config' => [
+        'type'     => 'anomaly.field_type.select',
+        'disabled' => 'edit',
+        'config'   => [
             'options' => [
                 'private' => 'anomaly.extension.local_storage_adapter::configuration.privacy.option.private',
                 'public'  => 'anomaly.extension.local_storage_adapter::configuration.privacy.option.public'
