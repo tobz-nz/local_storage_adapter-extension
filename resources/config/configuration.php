e9a6abb4f0701c7fd24999fe74e8b6dad9cd0ee1
Ryan Thompson <service@aiwebsystems.com>
2015-08-07 18:20:53 -0500

    required

--- resources/config/configuration.php
+++ resources/config/configuration.php
@@ -2,8 +2,9 @@
 
 return [
     'privacy' => [
-        'type'   => 'anomaly.field_type.select',
-        'config' => [
+        'type'     => 'anomaly.field_type.select',
+        'required' => true,
+        'config'   => [
             'options' => [
                 'public'  => 'anomaly.extension.local_storage_adapter::configuration.privacy.option.public',
                 'private' => 'anomaly.extension.local_storage_adapter::configuration.privacy.option.private'
